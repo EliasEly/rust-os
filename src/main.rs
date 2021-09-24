7b66d8a9f69652b88e5bef03373b6612defd461d
Elias <elias.el@outlook.fr>
2021-09-24 22:09:46 +0200

    add: the start of rust-os
    The files contain the necessary code to test the code, write the result
    in the console and write in the VGA buffer.

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(rust_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use core::panic::PanicInfo;
+use rust_os::println;
+use rust_os::print;
+
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    rust_os::test_panic_handler(info)
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+
+    print!("Bottom Screen");
+
+    #[cfg(test)]
+    test_main();
+
+    loop {}
+}
